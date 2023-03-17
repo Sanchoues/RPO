42a0de5200d8a9586591b5ba7740bd0d70eee1e3
Alexander Eliseev <gaika.81.567@gmail.com>
2023-03-17 12:23:11 +0300

    Lab2

--- fclient/app/src/main/java/ru/iu3/fclient/MainActivity.java
+++ fclient/app/src/main/java/ru/iu3/fclient/MainActivity.java
@@ -1,17 +1,46 @@
 package ru.iu3.fclient;
 
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.crashlytics.buildtools.reloc.org.apache.commons.codec.DecoderException;
+import com.google.firebase.crashlytics.buildtools.reloc.org.apache.commons.codec.binary.Hex;
+
 
 public class MainActivity extends AppCompatActivity {
+    ActivityResultLauncher activityResultLauncher;
 
     // Used to load the 'native-lib' library on application startup.
     static {
         System.loadLibrary("native-lib");
         System.loadLibrary("mbedcrypto");
 
+    }
+
+
+    public static byte[] stringToHex(String s)
+    {
+        byte[] hex;
+        try
+        {
+            hex = Hex.decodeHex(s.toCharArray());
+        }
+        catch (DecoderException ex)
+        {
+            hex = null;
+        }
+        return hex;
     }
 
     @Override
@@ -21,9 +50,41 @@ public class MainActivity extends AppCompatActivity {
         int res =initRng();
         byte[] v = randomBytes(10);
         // Example of a call to a native method
-        TextView tv = findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
+     //   TextView tv = findViewById(R.id.sample_text);
+      //  tv.setText(stringFromJNI());
+
+        activityResultLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>()
+                {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        if (result.getResultCode() == Activity.RESULT_OK) {
+                            Intent data = result.getData();
+                            // обработка результата
+                            String pin = data.getStringExtra("pin");
+                            Toast.makeText(MainActivity.this, pin, Toast.LENGTH_SHORT).show();
+
+                        }
+                    }
+                });
     }
+
+    public void onButtonClick(View v)
+    {
+        Toast.makeText(this, "Hello", Toast.LENGTH_SHORT).show();
+        byte[] key = stringToHex("0123456789ABCDEF0123456789ABCDE0");
+        byte[] enc = encrypt(key, stringToHex("000000000000000102"));
+        byte[] dec = decrypt(key, enc);
+        String s = new String(Hex.encodeHex(dec)).toUpperCase();
+        Toast.makeText(this, s, Toast.LENGTH_SHORT).show();
+        Intent it = new Intent(this, PinpadActivity.class);
+        startActivity(it);
+    }
+
+    public static native byte[] encrypt(byte[] key, byte[] data);
+    public static native byte[] decrypt(byte[] key, byte[] data);
+
 
     /**
      * A native method that is implemented by the 'native-lib' native library,
--- libs/mbedtls/build/compile.sh
+++ libs/mbedtls/build/compile.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-#ABI=armeabi-v7a
+ABI=armeabi-v7a
 #ABI=x86
 #ABI=arm64-v8a
-ABI=x86_64
+#ABI=x86_64
 
-ANDROID_NDK=$HOME/Android/Sdk/ndk
+ANDROID_NDK=/home/username/Android/Sdk/ndk
 TOOL_CHAIN=${ANDROID_NDK}/23.1.7779620/build/cmake/android.toolchain.cmake
-CMAKE=$HOME/Android/Sdk/cmake/3.22.1/bin/cmake
+CMAKE=/home/username/Android/Sdk/cmake/3.22.1/bin/cmake
 
 mkdir -p ${ABI}
 cd ${ABI}
